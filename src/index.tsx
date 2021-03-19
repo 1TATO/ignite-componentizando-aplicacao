f44e0b841af328bf2d6ebed8db4695fe08319c29
José Neto <josenetopr5@gmail.com>
2021-03-19 15:20:23 -0300

    Challenge completed

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
 import { render } from 'react-dom'
+import { MoviesProvider } from './hooks/useMovies'
 
 import { App } from './App'
 
-render(<App />, document.getElementById('root'))
+render(
+  <MoviesProvider>
+    <App />, 
+  </MoviesProvider>,
+  document.getElementById('root')
+)
